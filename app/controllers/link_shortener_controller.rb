60f6902e4d405320ea4d9f66cef294ff447f039e
Gorbachev E <volartm@gmail.com>
2018-08-05 20:15:20 +0300

    hash added

--- app/controllers/link_shortener_controller.rb
+++ app/controllers/link_shortener_controller.rb
@@ -1,5 +1,26 @@
 class LinkShortenerController < ApplicationController
   def shorten
-    puts("link - " + params['link'])
+    link = params['link']
+    link.to_s.downcase!
+
+    unless link.nil?
+      sql = "SELECT\nlink\nFROM\npublic.links\nWHERE\nlinks.link ="
+      sql += "'" + link + "'" + "\n"
+      sql += "ORDER BY\nlinks.link\nASC\nLIMIT 1;"
+
+      records_array = ActiveRecord::Base.connection.exec_query(sql)
+
+      if records_array.empty?
+        new(link)
+      end
+
+    end
+  end
+
+  def new data
+    m = Link.new
+    m.link = data
+    m.link_hash = Base64.urlsafe_encode64(data)[0...6]
+    m.save
   end
 end
