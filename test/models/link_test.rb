27f0381141eeec70e0e5e99261e4338cb692b0b9
Gorbachev E <volartm@gmail.com>
2018-08-06 16:49:09 +0300

    simple test

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid link" do
+    record = Link.new
+    record.link = "http://ya.ru"
+    assert record.save
+  end
+
+  test "invalid link" do
+    record = Link.new
+    record.link = "ya.ru"
+    assert_not record.save
+  end
+
+
 end
